778c04e0ac19dfd36fe4320b9a6842177c7b5ffd
TylerWStroud <tyler.wyatt.stroud@gmail.com>
2025-02-13 20:18:24 -0500

    updated method

--- src/LinkedList_No_HT.java
+++ src/LinkedList_No_HT.java
@@ -1,4 +1,165 @@
-package PACKAGE_NAME;
+// Name: Tyler Stroud
+// Class: CS 3305/section W01
+// Term: Spring 2025
+// Instructor: Emin Mary Abraham
+// Assignment: 3
+public class LinkedList_No_HT
+{
+    public Node ListName;
 
-public class LinkedList_No_HT {
+    //constructor method to create a list object with a specifc name.
+    public LinkedList_No_HT()
+    {
+        ListName = null;
+    }
+
+    //method add node to end of list
+    public void addLastNode(int data)
+    {
+        if (ListName == null)
+            ListName = new Node(data); //one node list
+        else
+        {
+            Node temp = ListName;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+
+            temp.next = new Node(data); //link new node as last node
+        }
+    }
+
+    //======== Your part to complete for this assignment =========
+
+    //method #1: add first node
+    public void addFirstNode(int data)
+    {
+        // swaps new data node with the ListName 'header' node index
+        Node firstNode = new Node(data);
+        firstNode.next = ListName;
+        ListName = firstNode;
+    }
+
+
+    //method #2: add node at index
+    public void addAtIndex(int index, int data)
+    {
+        // in case requested index is 0
+        if(index == 0)
+            addFirstNode(data);
+        else
+        {
+            Node indexNode = new Node(data);
+
+            // list traversal to specified index
+            Node temp = ListName;
+            for(int i = 0; i < index-1; i++){
+                temp = temp.next;
+            }
+
+            // node swap
+            indexNode.next = temp.next;
+            temp.next=indexNode;
+        }
+    }
+
+
+    //method #3: remove first node
+    public void removeFirstNode() {
+            ListName = ListName.next;
+    }
+
+
+    //method #4: remove last node
+    public void removeLastNode()
+    {
+            Node temp = ListName;
+            // traversal to second-to-last node
+            while(temp.next.next != null){
+                temp = temp.next;
+            }
+            // makes last node null
+            temp.next = null;
+    }
+
+
+    //method #5: remove node at index
+    public void removeAtIndex(int index)
+    {
+        if(index == 0 )
+            removeFirstNode();
+        else
+        {
+            Node temp = ListName;
+            // node traversal
+            for(int i = 0; i < index-1; i++){
+                temp = temp.next;
+            }
+            // sets next node's data its next node's data
+            temp.next = temp.next.next;
+        }
+    }
+
+    //method #6: countNodes
+    public int countNodes()
+    {
+        int listSize= 0;
+        // checks nullity
+        if(ListName == null)
+            return listSize;
+        else
+        {
+            Node temp = ListName;
+            // initial element count
+            listSize++;
+            // traversal count
+            while (temp.next != null) {
+                listSize++;
+                temp = temp.next;
+            }
+        }
+        return listSize;
+    }
+
+
+    //method #7: printInReverse (must be a Recursive method)
+    public void printInReverseRecursive(Node L)
+    {
+        if(L != null){
+            // recursive traversal to end of list
+            printInReverseRecursive(L.next);
+            // print node data
+            System.out.print(L.data+"   ");
+        }
+    }
+
+    //================= end of your part ==============
+
+    //method to print out the list
+    public void printList()
+    {
+        Node temp;
+        temp = ListName;
+        while (temp != null)
+        {
+            System.out.print(temp.data + "   ");
+            temp = temp.next;
+        }
+    }
+
+    //class to create nodes as objects
+    private class Node
+    {
+        private int data;  //data field
+        private Node next; //link field
+
+        public Node(int item) //constructor method
+        {
+            data = item;
+            next = null;
+        }
+    }
 }
+
+
--- src/myTest_No_HT.java
+++ src/myTest_No_HT.java
@@ -1,4 +1,168 @@
-package PACKAGE_NAME;
+// Name: Tyler Stroud
+// Class: CS 3305/section W01
+// Term: Spring 2025
+// Instructor: Emin Mary Abraham
+// Assignment: 3
+import java.util.Scanner;
 
-public class myTest_No_HT {
+public class myTest_No_HT
+{
+    public static void main (String[] args) throws Exception
+    {
+        LinkedList_No_HT myList = new LinkedList_No_HT(); //create a list object
+        Scanner s = new Scanner(System.in); // scanner object
+        int input = 0; // menu input
+
+
+        try{
+            // MAIN MENU
+           while(input != 10){
+               System.out.print("\n\n---------MAIN MENU--------\n" +
+                       "1 – Add First Node\n" +
+                       "2 – Add Last Node\n" +
+                       "3 – Add At Index\n" +
+                       "4 – Remove First Node\n" +
+                       "5 – Remove Last Node\n" +
+                       "6 – Remove At Index\n" +
+                       "7 – Print List Size\n" +
+                       "8 – Print List Forward\n" +
+                       "9 – Print List In Reverse\n" +
+                       "10- Exit program\n\n" +
+                       "Enter option number:");
+               input = Integer.parseInt(s.nextLine());
+
+               // Menu functions
+               switch(input){
+                   /* addFirstNode */
+                   case 1:
+                       System.out.print("\nEnter the node data you want to add: ");
+                       int firstNode = Integer.parseInt(s.nextLine());
+                       System.out.println("\nMethod addFirstNode()");
+                       System.out.print("List content before adding "+firstNode+" is:\t");
+                       myList.printList();
+                       myList.addFirstNode(firstNode);
+                       System.out.print("\nList content after adding "+firstNode+" is:\t\t");
+                       myList.printList();
+                       System.out.println();
+                       break;
+
+                   /* addLastNode */
+                   case 2:
+                       System.out.print("\nEnter the node data you want to add: ");
+                       int lastNode = Integer.parseInt(s.nextLine());
+                       System.out.println("\nMethod addLastNode()");
+                       System.out.print("List content before adding "+lastNode+" is:\t");
+                       myList.printList();
+                       myList.addLastNode(lastNode);
+                       System.out.print("\nList content after adding "+lastNode+" is:\t");
+                       myList.printList();
+                       System.out.println();
+                       break;
+
+                   /* addAtIndex */
+                   case 3:
+                       System.out.print("\nWhat index would you like your new node?: ");
+                       int index = Integer.parseInt(s.nextLine());
+
+                       // invalid index catch
+                       while(index < 0 || index >= myList.countNodes())
+                       {
+                           System.out.print("\nInvalid index, try again.\nWhat index would you like your new node: ");
+                           index = Integer.parseInt(s.nextLine());
+                       }
+
+                       System.out.print("\nEnter the node data you want to add: ");
+                       int indexNode = Integer.parseInt(s.nextLine());
+                       System.out.println("\nMethod addAtIndex()");
+                       System.out.print("List content before adding "+indexNode+" is:\t");
+                       myList.printList();
+                       myList.addAtIndex(index,indexNode);
+                       System.out.print("\nList content after adding "+indexNode+" is:\t");
+                       myList.printList();
+                       System.out.println();
+                       break;
+
+                   /* removeFirstNode */
+                   case 4:
+                       // empty list catch
+                       if(myList.countNodes() == 0) {
+                           System.out.println("List is Empty.");
+                           break;
+                       }
+
+                       System.out.println("\nMethod removeFirstNode()");
+                       System.out.print("List content before removing first node is:\t\t");
+                       myList.printList();
+                       myList.removeFirstNode();
+                       System.out.print("\nList content after removing first node is:\t\t");
+                       myList.printList();
+                       break;
+
+                   /* removeLastNode */
+                   case 5:
+                       // empty list catch
+                       if(myList.countNodes() == 0) {
+                           System.out.println("List is Empty.");
+                           break;
+                       }
+
+                       System.out.println("\nMethod removeLastNode()");
+                       System.out.print("List content before removing last node is:\t");
+                       myList.printList();
+                       myList.removeLastNode();
+                       System.out.print("\nList content after removing last node is:\t");
+                       myList.printList();
+                       break;
+                       
+                   /* removeAtIndex */
+                   case 6:
+                       // empty list catch
+                       if(myList.countNodes() == 0) {
+                           System.out.println("List is Empty.");
+                           break;
+                       }
+                       System.out.print("Which index do you want to remove?: ");
+                       int removeIndex = Integer.parseInt(s.nextLine());
+                       
+                       // invalid index catch
+                       while(removeIndex < 0 || removeIndex >= myList.countNodes()){
+                           System.out.print("\nInvalid index, try again.\nWhat index would you to remove?: ");
+                           removeIndex = Integer.parseInt(s.nextLine());
+                       }
+                       
+                       System.out.println("\nMethod removeAtIndex()");
+                       System.out.print("List content before removing node at index "+removeIndex+" is:\t");
+                       myList.printList();
+                       myList.removeAtIndex(removeIndex);
+                       System.out.print("\nList content after removing node at index "+removeIndex+" is:\t\t");
+                       myList.printList();
+                       break;
+                       
+                   /* print list size */
+                   case 7:
+                       System.out.println("\nMethod countNodes()" +
+                               "\nThere are "+myList.countNodes()+" nodes in your list.");
+                       break;
+
+                   /* print list forward */
+                   case 8: 
+                       System.out.println("\nMethod printList()");
+                       System.out.print("Here is your list:\t");
+                       myList.printList();
+                       break;
+
+                   /* print list in reverse */
+                   case 9:
+                       System.out.println("\nMethod printInReverseRecursive()");
+                       System.out.print("Here is your list:\t");
+                       myList.printInReverseRecursive(myList.ListName);
+                       break;
+               }
+           }
+           System.out.println("\nExiting...");
+        }catch(NumberFormatException e){
+            System.out.println("\nInvalid menu option, please choose between options 1-10");
+        }
+
+    }
 }
